92bbb742bc00ca7dd178c87e20e4e66e3e45cd38
Seth Bernard <sethbernard7@gmail.com>
2020-02-21 20:11:22 -0500

    Add ability to toggle a mobile menu and add more media queries

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -44,3 +44,18 @@ contactForm.addEventListener('submit', handleForm);
 // Get current year for footer
 let date = new Date().getFullYear();
 document.getElementById('current-year').innerHTML = date;
+
+//Mobile menu
+const mainMenu = document.querySelector('.main-menu');
+const mobileMenuButton = document.querySelector('.mobile-menu-button');
+const menuItems = document.querySelectorAll('nav ul li a');
+
+mobileMenuButton.addEventListener('click', () => {
+  mainMenu.classList.toggle('mobile-menu');
+});
+
+menuItems.forEach((item) => {
+  item.addEventListener('click', () => {
+    mainMenu.classList.toggle('mobile-menu');
+  });
+});
