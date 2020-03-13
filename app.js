370e39aab0ab161d00c8f57919c44fbe5ed05cad
Seth Bernard <sethbernard7@gmail.com>
2020-03-13 13:57:57 -0400

    Add 'postinstall' script

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 require('dotenv').config();
 const { check, validationResult } = require('express-validator');
