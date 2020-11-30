13fd2a8482c3182d886a0713cf1c7fa44427b2de
Seth Bernard <sethbernard7@gmail.com>
2020-11-30 16:28:54 -0500

    Remove js that toggles nav based on scroll direction

--- src/js/navAndFooter.js
+++ src/js/navAndFooter.js
@@ -4,26 +4,13 @@ const mobileMenuButton = document.querySelector('.mobile-menu-button');
 const menuItems = document.querySelectorAll('nav ul li a');
 
 mobileMenuButton.addEventListener('click', () => {
-  mainMenu.classList.toggle('mobile-menu');
+    mainMenu.classList.toggle('mobile-menu');
 });
 
 menuItems.forEach((item) => {
-  item.addEventListener('click', () => {
-    mainMenu.classList.toggle('mobile-menu');
-  });
-});
-
-// Make navbar appear on scroll up and hide on scroll down
-let prevScrollPos = window.pageYOffset;
-
-window.addEventListener('scroll', () => {
-  let currentScrollPos = window.pageYOffset;
-  if (prevScrollPos > currentScrollPos) {
-    document.querySelector('nav').style.top = '0';
-  } else {
-    document.querySelector('nav').style.top = '-69px';
-  }
-  prevScrollPos = currentScrollPos;
+    item.addEventListener('click', () => {
+        mainMenu.classList.toggle('mobile-menu');
+    });
 });
 
 // Get current year for footer
